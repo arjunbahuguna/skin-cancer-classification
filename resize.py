a08f73aed7f5d285179a7bf6cc61513c361c07cd
Arjun Bahuguna <arjun.abraxas@gmail.com>
2017-09-30 03:48:53 +0530

    Update resize.py

--- resize.py
+++ resize.py
@@ -8,11 +8,11 @@ height_of_images=[]
 width_of_images=[]
 
 def find_avg_dim(folder):
-  	#read files from folder
+  #read files from folder
     for filename in os.listdir(folder):
-	    img = cv2.imread(os.path.join(folder,filename),0)    #accepts grayscale input, remove 0 to take RBG input
-	    if img is not None:
-        	#extract height and width
+		img = cv2.imread(os.path.join(folder,filename),0)    #accepts grayscale input, remove 0 to take RBG input
+		if img is not None:
+        		#extract height and width
 			height, width = img.shape			
 			height_of_images.append(height)		
 			width_of_images.append(width)
@@ -27,7 +27,7 @@ def resize(folder):
 	for filename in os.listdir(folder):
 		img = cv2.imread(os.path.join(folder,filename),0)
 		if img is not None:
-	       	#edit
+	       		#edit
 			resized = cv2.resize(img, (avg_width, avg_height), interpolation = cv2.INTER_AREA)
-	        #output
+	        	#output
 			cv2.imwrite(os.path.join(folder,filename),resized)
